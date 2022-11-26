0341b0fc3575fdfdb6798c2d2d0caba4aeb54c0c
Tomasz Margalski <tomasz.margalski@hotmail.com>
2022-11-26 12:52:03 +0100

    x

--- hack.js
+++ hack.js
@@ -1,26 +1,16 @@
 var code = new URLSearchParams(window.location.search).get("code");
 
-var questions = [];
+fetch(
+  `https://st6arucqsa.execute-api.eu-west-1.amazonaws.com/production/pulses/questions/${code}`
+)
+  .then((res) => res.json())
+  .then((json) => {
+    setInterval(() => {
+      answerQuestions(json.content.questions);
+    }, 200);
+  });
 
-var xhr = new XMLHttpRequest();
-xhr.onload = function () {
-  var json = xhr.responseText;
-  json = json.replace(/^[^(]*\(([\S\s]+)\);?$/, "$1");
-  json = JSON.parse(json);
-
-  questions = json.content.questions;
-
-  setInterval(answerQuestions, 200);
-};
-
-xhr.open(
-  "GET",
-  "https://st6arucqsa.execute-api.eu-west-1.amazonaws.com/production/pulses/questions/" +
-    code
-);
-xhr.send();
-
-function answerQuestions() {
+function answerQuestions(questions) {
   var question = questions.find((o) => {
     return o["name"] === $(".m-questions:visible h1").text();
   });
@@ -28,6 +18,6 @@ function answerQuestions() {
     return o["value"] === 100;
   });
   $(
-    ".m-questions:visible .answer-options:contains('" + bestAnswer.lable + "')"
+    `.m-questions:visible .answer-options:contains(${bestAnswer.lable})`
   ).click();
 }
